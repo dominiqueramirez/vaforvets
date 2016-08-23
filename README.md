656bdf2fa5afb88da28fd7c0db24501e8ff0c932
Stephanie Green <steph.green44@gmail.com>
2016-08-23 11:51:30 -0400

    removed directories

--- README.md
+++
@@ -1,4 +0,0 @@
-# vaforvets
-Responsive Redesign of VAforVets Website
-
-http://vaforvets.va.gov/
--- va-home-April052016-updated copy/js/jquery-validate-methods.js
+++
@@ -1,5 +0,0 @@
-﻿// US ZipCode
-jQuery.validator.addMethod("zipcodeUS", function(value, element) {
-    return this.optional(element) || /\d{5}-\d{4}$|^\d{5}$/.test(value);
-}, "Invalid US ZIP Code.");
-
