38f0f6e536cbe46e17fc1142f959263e6a480785
Stephanie Green <steph.green44@gmail.com>
2016-08-26 10:41:58 -0400

    updated files upload

---
+++ js/jquery-validate-methods.js
@@ -0,0 +1,5 @@
+﻿// US ZipCode
+jQuery.validator.addMethod("zipcodeUS", function(value, element) {
+    return this.optional(element) || /\d{5}-\d{4}$|^\d{5}$/.test(value);
+}, "Invalid US ZIP Code.");
+
